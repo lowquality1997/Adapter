ed379833e9f776e4027dad08a55efafbd55232ce
lowquality1997 <30823971+lowquality1997@users.noreply.github.com>
2017-11-14 10:42:18 +0700

    Initial commit

---
+++ src/MainApp.java
@@ -0,0 +1,12 @@
+
+class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LapTop mylabtop = new LapTop(new Adapter(new Power(220)));
+		mylabtop.powerOn();
+
+	}
+
+}
